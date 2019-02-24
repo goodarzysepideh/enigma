fa813dffe7b37e41cc9197c2eac39c2c47c2863b
goodarzysepideh <goodarzysepideh@gmail.com>
2019-02-24 01:51:14 -0700

    room A completed

--- app.py
+++ app.py
@@ -7,14 +7,6 @@ app = Flask(__name__)
 def first_page():
     return render_template('page1.html');
 
-@app.route('/page2')
-def retrun_page2():
-    return render_template('page2.html')
-
-@app.route('/page3')
-def retrun_page3():
-    return render_template('page3.html')    
-
 @app.route('/page1', methods=['POST'])
 def mpars():
     #count = get_hit_count()
@@ -23,6 +15,50 @@ def mpars():
         return 'page2', status.HTTP_200_OK
     else:
         return 'alaki', status.HTTP_400_BAD_REQUEST
+
+@app.route('/page2')
+def retrun_page2():
+    return render_template('page2.html')
+
+@app.route('/page3')
+def retrun_page3():
+    return render_template('page3.html')
+
+@app.route('/page4')
+def retrun_page4():
+    return render_template('page4.html')
+
+@app.route('/page4p1', methods=['POST'])
+def parsdecripted():
+    print("look here:" , request.form['result'])
+    if request.form['result'] == 'the rooms are linked':
+        return 'page5', status.HTTP_200_OK
+    else:
+        return 'alaki', status.HTTP_400_BAD_REQUEST
+
+@app.route('/page5')
+def retrun_page5():
+    return render_template('page5.html')
+
+@app.route('/page4p2', methods=['POST'])
+def parsHash():
+    print("look here:" , request.form['result'])
+    if request.form['result'] == '3C14D7C01FFB526638B434E4FD0E13AF':
+        return 'page6', status.HTTP_200_OK
+    else:
+        return 'alaki', status.HTTP_400_BAD_REQUEST
+
+@app.route('/page4p3', methods=['POST'])
+def parsHash3():
+    print("look here:" , request.form['result'])
+    if request.form['result'] == 'pfpZ4Xayj7fNwIu2bDTL4UbwAewk1datIwLU9wZ/aH6yIMsuf4/WohWisPGBseWJzohDDCAjpQRUbYtQwjAzZ0W1w9AN21A2aKcks6p7uT8kafysKQM+I4fkL3/N6d1U03wYD6xROcdEZE9So9i0JRv8KncgN98xEAbai3CjZNQ=':
+        return 'page7', status.HTTP_200_OK
+    else:
+        return 'alaki', status.HTTP_400_BAD_REQUEST
+
+@app.route('/page6')
+def retrun_page6():
+    return render_template('page6.html')
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000 , debug=True)
