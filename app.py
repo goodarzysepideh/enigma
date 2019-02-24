9eef6c6a41295e25c8ba7b3e1d4a282000bcba90
goodarzysepideh <goodarzysepideh@gmail.com>
2019-02-23 19:41:48 -0700

    finished page1

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+from flask_api import status
+
+app = Flask(__name__)
+
+@app.route('/')
+def first_page():
+    return render_template('page1.html');
+
+@app.route('/page2')
+def retrun_page2():
+    return render_template('page2.html', error=error)
+
+@app.route('/page1', methods=['POST'])
+def mpars():
+    #count = get_hit_count()
+    print("look here:" , request.form['result'])
+    if request.form['result'] == 'No beginning! No ending!':
+        return 'page2', status.HTTP_200_OK
+    else:
+        return 'alaki', status.HTTP_400_BAD_REQUEST
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000 , debug=True)
